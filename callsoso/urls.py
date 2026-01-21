b85d0eec6c716f25eef983cff74705e44cedeadf
CreepyLegend57 <manyudzajoe2018@gmail.com>
2026-01-21 14:29:06 +0200

    Fix settings for local + production

--- callsoso/urls.py
+++ callsoso/urls.py
@@ -14,21 +14,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
+URL configuration for callsoso project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+"""
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
 
-    # Website (public)
-    path('', include('website.urls')),
+    # Public website (core pages, news, knowledge center, auth, features)
+    path('', include(('website.urls', 'website'), namespace='website')),
 
-    # Directory (private)
-    path('directory/', include('directory.urls')),
+    # Directory (private dashboard, surplus/demand/matches)
+    path('directory/', include(('directory.urls', 'directory'), namespace='directory')),
 ]
 
+# Serve static & media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
